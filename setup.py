ff87460b4b5998b2ffca44bd56a85c07545d5643
Walter Danilo Galante <walterdangalante@gmail.com>
2015-03-31 16:47:31 -0700

    Version 0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name='pymorm',
-    version='0.2.1',
+    version='0.2.2',
     description='Really simple pymongo-based ORM',
     author='Walter Danilo Galante',
     author_email='walter.galante@proxtome.com',
